34eb042ee337dbc384a56f48916f80ea9dc2ce36
JayGarxP <jpfallon4@gmail.com>
2017-12-14 18:03:16 -0800

    Getting correct answer

--- GeeksForGeeks/CountConnectedDictionaryValues/CountConnectedDictionaryValues/Program.cs
+++ GeeksForGeeks/CountConnectedDictionaryValues/CountConnectedDictionaryValues/Program.cs
@@ -17,7 +17,7 @@ namespace CountConnectedDictionaryValues
 
         public class Employee
         {
-            
+
             public string ID { get; set; }
             public List<string> reportingIDs { get; set; }
 
@@ -30,6 +30,8 @@ namespace CountConnectedDictionaryValues
             public Employee()
             {
                 reportingIDs = new List<string>();
+                directPeonCount = 0;
+                totalPeonCount = 0;
             }
         };
 
@@ -46,7 +48,7 @@ namespace CountConnectedDictionaryValues
     };
             //CEO reports to herself and CALLS THE SHOTS! YA FIYAD!
             string BigBossID;
-          
+
             IDictionary<string, int> output = new Dictionary<string, int>(); // <BOSS id>, <Henchmen count>
                                                                              //https://stackoverflow.com/questions/1276763/how-do-i-get-the-list-of-keys-in-a-dictionary
             List<string> CGEmployees = dict.Keys.ToList();
@@ -57,7 +59,7 @@ namespace CountConnectedDictionaryValues
                 output.Add(EmpId, 0);
                 Employee staff = new Employee();
 
-                    staff.ID = EmpId; //staff ID is key to be used as boss
+                staff.ID = EmpId; //staff ID is key to be used as boss
                 if (EmpId == dict[EmpId])
                 {//Find BigBoss who reports to no one, not the IRS, not their family, not even God.
                     BigBossID = EmpId;
@@ -68,27 +70,57 @@ namespace CountConnectedDictionaryValues
 
             //Built staff w/ only IDs by switching dictionary Keys to Values sort of
             //Now find collisions and trace them until no reporting values are found and update the data in Employee correctly as you go.
-            foreach (Employee staff in Company) {
+            foreach (Employee staff in Company)
+            {
                 Console.WriteLine("Hi I'm " + staff.ID);
-                foreach (KeyValuePair<string,string> bossID in dict) {
-                    if (staff.ID == bossID.Value) {
-                        Console.WriteLine(bossID.Value +" is the boss of: "+ bossID.Key);
-
+                foreach (KeyValuePair<string, string> bossID in dict)
+                {
+                    if (staff.ID == bossID.Value)
+                    {
+                        Console.WriteLine(bossID.Value + " is the boss of: " + bossID.Key);
+                        if (bossID.Key != bossID.Value)
+                        {
+                            staff.directPeonCount++;
+                            staff.totalPeonCount++;
+                            staff.reportingIDs.Add(bossID.Key);
+                        }
                     }
                 }
 
-//                foreach (KeyValuePair<string, book> b in books) // or foreach(book b in books.Values)
-//                {
-//                    if (b.Value.title.Equals("some title", StringComparison.CurrentCultureIgnoreCase))
-//                        return true
-//}
+                //https://stackoverflow.com/questions/10436813/check-if-value-already-exists
+                //                foreach (KeyValuePair<string, book> b in books) // or foreach(book b in books.Values)
+                //                {
+                //                    if (b.Value.title.Equals("some title", StringComparison.CurrentCultureIgnoreCase))
+                //                        return true
+                //}
+            }
+            foreach (Employee staff in Company) {
+                if (staff.directPeonCount > 0) {
+                    foreach (string peon in staff.reportingIDs) {
+                        //staff.totalPeonCount += reporting staff peon count(s)
+
+                        //LINQ
+                          var reportingIN  = Company.FirstOrDefault(o => o.ID == peon);
+                        if (peon != null && reportingIN.ID != staff.ID)
+                        { //don't double count BigBoss 
+                            staff.totalPeonCount += reportingIN.directPeonCount;
+                        }
+                    }
+                }
+
+            }
+
+            foreach (Employee staff in Company) {
+                Console.WriteLine("Ya boy "+staff.ID+" has "+staff.totalPeonCount.ToString()+" henchmen beneath them.");
+               
             }
 
             Console.WriteLine("AI will have the capacity to suffer");
         }
 
 
-        bool TraverseDictionary(List<String> ids, List<Employee> employees) {
+        bool TraverseDictionary(List<String> ids, List<Employee> employees)
+        {
 
             return false;
         }
