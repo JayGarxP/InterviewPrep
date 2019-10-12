ff79b8b3b2d554514422f987d04759708a474a72
JP Fallon <JayGarxP@users.noreply.github.com>
2019-10-11 17:55:56 -0700

    finish IQR of Frequency Map

--- HackerRank/DataScience/TenDays-DataScience-1-2.cs
+++ HackerRank/DataScience/TenDays-DataScience-1-2.cs
@@ -12,15 +12,20 @@ class Solution {
     static void Main(String[] args) {
         int N = Int32.Parse(Console.ReadLine());
         string xString = Console.ReadLine();
+        string fString = Console.ReadLine();
         string[] Xstringsss = xString.Split(' ');
-        List<int> X = new List<int>();
+        string[] Fstringsss = fString.Split(' ');
+       List<int> S = new List<int>();
         for(int i = 0; i < Xstringsss.Length; i++)
         {
         try
        {
             int numVal = Int32.Parse(Xstringsss[i]);
-            //Console.WriteLine(numVal);
-            X.Add(numVal);
+            int freqVal = Int32.Parse(Fstringsss[i]);
+            for(int j = 0; j < freqVal; j++)
+            {
+                S.Add(numVal);
+            }
        }
        catch (FormatException e)
        {
@@ -29,16 +34,12 @@ class Solution {
         }
 
 // default List<numeric>.Sort() is ascending order
-X.Sort();
-int[] AscendingData = X.ToArray();
-
-int medianIndex = N / 2;
-
-Console.WriteLine(CalculateMedian(AscendingData, 0, N - 1));
-Console.WriteLine(CalculateMedian(AscendingData, medianIndex, N - 1));
+ S.Sort();
+int[] AscendingData = S.ToArray();
 
 
-double iqr = CalcIQR(AscendingData, N);
+
+double iqr = CalcIQR(AscendingData, AscendingData.Length);
 
 if(iqr % 1 == 0)
 Console.WriteLine(iqr.ToString() + ".0");
@@ -54,37 +55,27 @@ Console.WriteLine(iqr);
 public static double CalcIQR(int[] ASC_DataSet, int elemCount)
 {
     double iqr = 0.0;
+     int medianIndex = elemCount / 2;
 
-//      bool oddDataSetSize = true;
-//         if(ASC_DataSetSize % 2 == 0)
-//         {
-//             // evenly divides by two; set element count is EVEN
-//             oddDataSetSize = false;
-//         }
+     bool oddDataSetSize = true;
+        if(elemCount % 2 == 0)
+        {
+            // evenly divides by two; set element count is EVEN
+            oddDataSetSize = false;
+        }
 
-// // If there is an odd number of data points in the original ordered data set, do not include the median (the central value in the ordered list) in either half.
-//         if(oddDataSetSize)
-//         {
-            
-//         }else
-//         {
-//             // If there is an even number of data points in the original ordered data set, split this data set exactly in half.
-
-//         }
-
-    int medianIndex = elemCount / 2;
-
-// even set size, don't exclude median; odd set size EXCLUDE median
-    if(elemCount % 2 == 0)
-    {
-        // IQR == Q3 - Q1
-       
-    }else
-    {
-       
-    }
-
-    
+// If there is an odd number of data points in the original ordered data set, do not include the median (the central value in the ordered list) in either half.
+        if(oddDataSetSize)
+        {
+            // IQR = Q3 - Q1;
+            iqr = CalculateMedian(ASC_DataSet, medianIndex + 1, elemCount - 1) 
+            - CalculateMedian(ASC_DataSet, 0, medianIndex - 1);
+        }else
+        {
+            // If there is an even number of data points in the original ordered data set, split this data set exactly in half.
+               iqr = CalculateMedian(ASC_DataSet, medianIndex, elemCount - 1) 
+            - CalculateMedian(ASC_DataSet, 0, medianIndex - 1);
+        }
 
     return Math.Round(iqr, 1);
 }
