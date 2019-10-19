d0c8851ebc549245072db3346ba3936cea79d225
JP Fallon <JayGarxP@users.noreply.github.com>
2019-10-18 17:42:30 -0700

    0! == 1 NOT zero was culprit of "rounding" errors

--- HackerRank/DataScience/TenDays-DataScience-4-1.cs
+++ HackerRank/DataScience/TenDays-DataScience-4-1.cs
@@ -2,124 +2,57 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 class Solution {
+    // To fix rounding errors caused by bad factorial function,
+    // stole function and 0 index logic from RodneyShag    
     static void Main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-        double chanceBoy = 1.09;
-        double chanceGirl = 1;
-// binomial distribution vars
-int x, n;
-        double p, q;
-
-// 1 child born per birth
-// what proportion of Russian fam with 6 children have AT LEAST 3 boys?
-
-x = 3;
-n = 6;
-// p(boy) = boychance / chancegirl + chance boy 
-// just that over everything it can be
-p = chanceBoy / (1 + chanceBoy);
-//Console.WriteLine("P is " + p.ToString());
-q = 1 - p;
-//Console.WriteLine("Q is " + q.ToString());
-
-
-// checkout this cool factorial snippet kids!
-// http://www.luschny.de/math/factorial/csharp/FactorialPoorMans.cs.html
-
-//binomial distro b(x,n,p) ==
-// (n!/((x!)*((n-x)!))  *  p^x   * q^(n-x)
-
-
-double binDist = 0;
-
-// accumulate value of binDist x times for "AT LEAST"
-  //for (int x = 3; x <= n; x++) {
-   //         result += binomial(n, x, p);
-    //    }
-for(int i = x; i <= n; i++)
-{
- binDist +=
-    Combination(n, i) * 
-    Math.Pow(p, i) *
-Math.Pow(q, n - i);
-}
-
-
-Console.WriteLine(Math.Round(binDist, 3));
-// should get 0.6957
-    }
-// https://www.codeproject.com/Tips/262142/Calculate-the-Factorial-of-an-Intiger-in-Csharp
-public static int Factorial(int input)
-{
-    int answer = 0;
-    int count = 0;
-
-    if (input >= 0)
-    {
-        count = 1;
-        while (count <= input)
-        {
-            if (count == 1)
-            {
-                answer= 1;
-                count++;
-            }
-            else
-            {
-                answer = count * answer;
-                count++;
-            }
+           double ratio = 1.09;
+        
+        double p = ratio / (1 + ratio);
+        int    n = 6;
+        
+        /* Calculate result */
+        double result = 0;
+        for (int x = 3; x <= n; x++) {
+            result += binomial(n, x, p);
         }
+
+        Console.WriteLine(Math.Round(result, 3));
+        
     }
 
-    return answer;
-}
-
-// public static double Combination(int nObjects, int rSampleSize)
-// {
-
-// int nFacto = Factorial(nObjects);
-// //Console.WriteLine(nFacto);
-// int xFacto = Factorial(rSampleSize);
-// //Console.WriteLine(xFacto);
-
-// int nxDiffFacto = Factorial((nObjects - rSampleSize));
-// //Console.WriteLine(nxDiffFacto);
-
-// double Combinations =  
-// (   
-// ((double)(nFacto)) 
-// / 
-// (   (double)( ((double)xFacto) * ((double)nxDiffFacto))    )
-// );
-
-// return Combinations;
-// }
-
-public static long Combination(int nObjects, int rSampleSize)
-{
-
-long nFacto = Factorial(nObjects);
-//Console.WriteLine(nFacto);
-long xFacto = Factorial(rSampleSize);
-//Console.WriteLine(xFacto);
-
-long nxDiffFacto = Factorial((nObjects - rSampleSize));
-//Console.WriteLine(nxDiffFacto);
-
-if(nxDiffFacto == 0)
-{
-    return 0;
-}
-
-long Combinations =  
-(   
-((nFacto)) 
-/ 
-(( (xFacto) * (nxDiffFacto) ))
-);
-
-return Combinations;
-}
+// return null in Java changed to return 0 in C#
+// there is no primitive vs special box Integer vs int in C# unlike Java,
+// so a C# value type can't be NULL, will exit early when x == n to not be off by one
+  private static double binomial(int n, int x, double p) {
+        if (p < 0 || p > 1 || n < 0 || x < 0 || x > n) {
+            return 0;
+        }
+        return combinations(n, x) * Math.Pow(p, x) * Math.Pow(1 - p, n - x);
+    }
+    
+    private static long combinations(int n, int x) {
+        if (n < 0 || x < 0 || x > n) {
+            return 0;
+        }
+        return factorial(n) / ( factorial(x) * factorial(n - x) );
+    }
+    
+    // This was the initial culprit, 
+    // the 0! == 1 edge case was missing from the factorial function I stole
+    // off a terrible website. This was the real cause of "rounding" errors
+    // Literally missed this EXACT edge case in a live programming interview BTW
+    // before making this mistake again here...
+    // 0! zero factorial is ONE!!! NOT ZERO!!!!
+    private static long factorial (int n) {
+        if (n < 0) {
+            return 0;
+        }
+        long result = 1;
+        while (n > 0) {
+            result *= n--;
+        }
+        return result;
+    }
 
 }
+
